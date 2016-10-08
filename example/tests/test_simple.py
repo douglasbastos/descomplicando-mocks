5a3b8d296faa967f0f485416a4dbf53566364306
Douglas Bastos <douglashsb@gmail.com>
2016-10-08 01:58:37 -0300

    Remove main no final do arquivo

--- example/tests/test_simple.py
+++ example/tests/test_simple.py
@@ -7,7 +7,3 @@ class SomaTest(unittest.TestCase):
     def test_soma_certa(self):
         result = soma(2, 2)
         self.assertEqual(result, 4)
-
-
-if __name__ == '__main__':
-    unittest.main()
