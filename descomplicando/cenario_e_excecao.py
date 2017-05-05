a460b9beff1cf5fd010cddb99f8cbe606db39210
Douglas Bastos <douglashsb@gmail.com>
2017-05-05 20:20:11 -0300

    Alterando cenário de exceção

--- descomplicando/cenario_e_excecao.py
+++ descomplicando/cenario_e_excecao.py
@@ -8,14 +8,15 @@ def tem_permissao(user, permissao):
     pass
 
 
-def data_proximo_pybr(user):
-    if tem_permissao(user=user, permissao='ver evento'):
+def ver_evento(user, evento='pythonbrasil'):
+    if tem_permissao(user=user, evento=evento):
         return 'De 06 a 11 de Outubro'
     raise SemPermissao
 
 
 def mostrar_data_da_proxima_pybr():
     try:
-        return data_proximo_pybr(user='douglasbastos')
+        return ver_evento(user='douglasbastos',
+                          evento='pythonbrasil')
     except SemPermissao:
         return 'Seu usuário não tem permissão'
--- tests/test_cenario_e_excecao.py
+++ tests/test_cenario_e_excecao.py
@@ -11,17 +11,17 @@ class ExibirDataPyBRTest(unittest.TestCase):
 
     def test_nao_deve_mostrar_evento_se_usuario_nao_tiver_permissao(self):
         with mock.patch.object(cenario_e_excecao,
-                               'tem_permissao',
+                               'ver_evento',
                                side_effect=SemPermissao):
             result = mostrar_data_da_proxima_pybr()
             self.assertEqual(result, 'Seu usuário não tem permissão')
 
     def test_deve_mostrar_evento_se_usuario_tiver_permissao(self):
         with mock.patch.object(cenario_e_excecao,
-                               'tem_permissao',
-                               return_value=True):
+                               'ver_evento',
+                               return_value='06 a 11 de Outubro'):
             result = mostrar_data_da_proxima_pybr()
-            self.assertEqual(result, 'De 06 a 11 de Outubro')
+            self.assertEqual(result, '06 a 11 de Outubro')
 
 if __name__ == '__main__':
     unittest.main()
